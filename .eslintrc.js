94034f4329d48e3f3a725ed92ad3924bace68c85
Nikita Lazurenko <naj040315@gmail.com>
2020-11-16 22:08:33 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
