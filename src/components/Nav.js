cb471f3f6a763afadba14e99b17fb449952d5ada
Nikita Lazurenko <naj040315@gmail.com>
2020-11-17 20:46:39 +0200

    styling Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,16 +4,20 @@ import styled from 'styled-components';
 const Nav = () => (
   <StyledNav>
     <h1>
-      <a href="#">Capture</a>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a id="logo" href="#">Capture</a>
     </h1>
     <ul>
       <li>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">1. About Us</a>
       </li>
       <li>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">2. Our Work</a>
       </li>
       <li>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">3. Contact Us</a>
       </li>
     </ul>
@@ -27,9 +31,27 @@ const StyledNav = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 1rem 10rem;
-  a{
+  background: #282828;
+
+  a {
     color: white;
     text-decoration: none;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  #logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+
+  li {
+    padding-left: 10rem;
+    position: relative;
   }
 `;
 
