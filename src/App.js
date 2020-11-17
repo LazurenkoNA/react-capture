1e07dff6a37bbff9350988e0d8f1c7efddc2e2e7
Nikita Lazurenko <naj040315@gmail.com>
2020-11-17 21:25:18 +0200

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+          <Route path="/work">
+            <OurWork />
+          </Route>
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <StyledNav>
     <h1>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a id="logo" href="#">Capture</a>
+      <Link id="logo" to="/">Capture</Link>
     </h1>
     <ul>
       <li>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">1. About Us</a>
+        <Link to="/">1. About Us</Link>
       </li>
       <li>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">2. Our Work</a>
+        <Link to="/work">2. Our Work</Link>
       </li>
       <li>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">3. Contact Us</a>
+        <Link to="/contact">3. Contact Us</Link>
       </li>
     </ul>
   </StyledNav>
